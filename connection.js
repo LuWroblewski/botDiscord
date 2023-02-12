96e2da0352167ef64e3885520b09fce20ab29730
LuWroblewski <wrolucas@gmail.com>
2023-02-12 17:01:50 -0300

    return update

--- connection.js
+++ connection.js
@@ -5,20 +5,10 @@ const db = monk(process.env.MONGO_URI);
 const collection = db.get('document')
 
 function connection() {
-    return db.then(() => {
-      console.log('Connected correctly to mongoDB');
 
-      db.on('open', () => {
-        console.log('Database connection opened');
-      });
-  
-      db.on('error', (error) => {
-        console.error(`Database error: ${error}`);
-      });
-
-      return db;
-
-    });
-  }
+  db.then(() => {
+    console.log('Connected correctly to mongoDB');
+  });
+}
   
 module.exports = {connection, collection}
